24cdb78ed458b268b237aac16584aa06d5366a73
Sylvain <90060355+Sylvain-Mani@users.noreply.github.com>
2023-05-10 00:45:08 +0200

    Ajout : Sidebar perso1

---
+++ js/sidebar_perso1-script.js
@@ -0,0 +1,22 @@
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e)=>{
+ let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+ arrowParent.classList.toggle("showMenu");
+  });
+}
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("close");
+});
+
+const tt = document.getElementById('plugins1');
+const tt2 = document.body;
+console.log(tt2);
+
+new ResizeObserver(entries => {
+  console.log(entries);
+}).observe(tt);
