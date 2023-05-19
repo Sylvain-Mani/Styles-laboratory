7758645e95590321de02479ddbd591af37cc58c9
Sylvain <90060355+Sylvain-Mani@users.noreply.github.com>
2023-05-19 22:22:45 +0200

    modif : pixel apparent, titre, data

--- js/Menu/menu1.js
+++ js/Menu/menu1.js
@@ -1,3 +1,10 @@
+const data = {
+    by: 'École du Web',
+    video: 'https://www.youtube.com/watch?v=-Qx-iHkeeD8',
+    code: 'https://codepen.io/Ziratsu/pen/eYpzdEQ'
+}
+
+
 const containerMenu = document.querySelector('.container-menu');
 const btnMenu = document.querySelector('.btn-menu');
 
